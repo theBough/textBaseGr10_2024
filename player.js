3523cb417fca8583d4c12ce134a664508a44114a
corbin Boughen <50217007+theBough@users.noreply.github.com>
2024-05-03 09:16:06 -0500

    Create player.js

---
+++ player.js
@@ -0,0 +1,12 @@
+function Player(x,y,w,h,col,img){
+  this.x = x;
+  this.y = y;
+  this.w = w;
+  this.h = h;
+  this.col = col;
+  this.img = loadImage(img);
+  
+  this.display = function(){
+    
+  }
+}//end player
