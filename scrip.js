8ecdd28cf46cd3bf125376629521fcf790c28004
corbin Boughen <50217007+theBough@users.noreply.github.com>
2024-05-03 09:16:42 -0500

    Update scrip.js

--- scrip.js
+++ scrip.js
@@ -1,1 +1,11 @@
-Code will show here
+let p;
+
+function setup() {
+  createCanvas(400, 400);
+  p = new Player(200,200,50,50,'#e76f51',"nothing")
+  
+}
+
+function draw() {
+  background('#2a9d8f');
+}
