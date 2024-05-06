11846e8ad5579376937013cad863f768fc0c8fda
corbin Boughen <50217007+theBough@users.noreply.github.com>
2024-05-06 08:42:59 -0500

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,13 @@
+let p;
+
+function setup() {
+  createCanvas(400, 400);
+  p = new Player(200,200,50,50,'#e76f51',"nothing")
+  
+}
+
+function draw() {
+  background('#2a9d8f');
+  p.display();
+  p.update();
+}
