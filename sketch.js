b1828950ba91df4452a7a85b5dece08f4d514e61
corbin Boughen <50217007+theBough@users.noreply.github.com>
2024-05-06 09:08:38 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,13 +1,17 @@
 let p;
+let myFont;
 
 function setup() {
   createCanvas(400, 400);
-  p = new Player(200,200,50,50,'#e76f51',"nothing")
-  
+  p = new Player(200,200,10,10,'#e76f51',"nothing")
+  myFont = loadFont("wght.ttf")
 }
 
 function draw() {
   background('#2a9d8f');
   p.display();
   p.update();
+  textFont(myFont)
+  textSize(50);
+  text("Adventure", 50,50)
 }
