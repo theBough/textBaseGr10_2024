08da1d764c25b2d3e4dafce5fe19ed835f246290
corbin Boughen <50217007+theBough@users.noreply.github.com>
2024-05-06 09:28:47 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,7 +6,6 @@ function setup() {
   p = new Player(200,200,10,10,'#e76f51',"nothing")
   myFont = loadFont("wght.ttf")
 }
-
 function draw() {
   background('#2a9d8f');
   p.display();
@@ -14,4 +13,13 @@ function draw() {
   textFont(myFont)
   textSize(50);
   text("Adventure", 50,50)
+  screenChange();
+}
+function screenChange(){
+  if(p.x + p.w < 0){
+    //The player has gone off the left side
+    p.x = width
+  }//end left boundry
+  
+  
 }
