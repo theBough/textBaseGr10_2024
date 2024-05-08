ef65c21f6b94f4895bf0e2e3729a7dfd72733bea
corbin Boughen <50217007+theBough@users.noreply.github.com>
2024-05-08 10:31:42 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,18 +1,18 @@
 let p;
 let w = [];
 let myFont;
-let activeRow, activeColumn;
+//let activeRow, activeColumn;
 
 function setup() {
   createCanvas(400, 400);
-  fillRooms();
+  //fillRooms();
   p = new Player(200, 200, 10, 10, "#e9c46a", "nothing");
-  w.push(new Wall(200,150, 20, 100, "#f4a261"));
-  w.push(new Wall(0, 0, 20, height, "#386641"));
-  w.push(new Wall(0, 0, width, 20, "#bc4749"));
+  
+  //change the words below to see other rooms
+  room2();
   myFont = loadFont("wght.ttf");
-  activeRow = 0;
-  activeColumn = 0;
+  //activeRow = 0;
+  // activeColumn = 0;
 }
 function draw() {
   background("#2a9d8f");
@@ -28,27 +28,37 @@ function draw() {
   textSize(50);
   text("Adventure", 50, 50);
   screenChange();
-   rooms[activeRow][activeColumn].call();
+  //rooms[activeRow][activeColumn].call();
 }
 function screenChange() {
   if (p.x + p.w < 0) {
     //The player has gone off the left side
     p.x = width;
-    activeColumn -=1
+    //activeColumn -=1
   } //end left boundry
   if (p.x > width) {
     //The player has gone off the right side
     p.x = 0;
-    activeColumn +=1
+    //activeColumn +=1
   } //end right boundry
   if (p.y > height) {
     //The player has gone off the bottom side
     p.y = 0;
-    activeRow += 1
+    activeRow += 1;
   } //end bottom boundry
   if (p.y < 0) {
     //The player has gone off the top side
     p.y = height;
-    activeRow -= 1
+    //activeRow -= 1
   } //end top boundry
+}
+function room1() {
+  w = [];
+  w.push(new Wall(200, 150, 20, 100, "#f4a261"));
+  w.push(new Wall(0, 0, 20, height, "#386641"));
+  w.push(new Wall(0, 0, width, 20, "#bc4749"));
+}
+function room2() {
+  w = [];
+  w.push(new Wall(200, 150, 20, 100, "#f4a261"));
 }
