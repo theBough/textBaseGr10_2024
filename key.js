81f3a5e1a243a97fd7327e1ccdee202771020590
corbin Boughen <50217007+theBough@users.noreply.github.com>
2024-05-17 09:41:58 -0500

    Update key.js

--- key.js
+++ key.js
@@ -1,15 +1,15 @@
-function Key(x,y,w,h,col,img){
+function Key(x, y, w, h, col, img) {
   this.x = x;
   this.y = y;
   this.w = w;
   this.h = h;
   this.col = col;
   this.img = loadImage(img);
-  
-  this.display = function(){
-    //fill(this.col)
-    //rect(this.x, this.y, this.w, this.h)
-    this.img.resize(this.w, this.h)
-    image(this.img, this.x, this.y)
-  }//end display
-}//end player
+
+  this.display = function () {
+    if (activeRow == 2 && activeColumn == 1) {
+      this.img.resize(this.w, this.h);
+      image(this.img, this.x, this.y);
+    }
+  }; //end display
+} //end player
