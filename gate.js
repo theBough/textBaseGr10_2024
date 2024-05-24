d8e54340886070968e3685c82bc983b75bb5f01c
corbin Boughen <50217007+theBough@users.noreply.github.com>
2024-05-24 09:29:07 -0500

    Update gate.js

--- gate.js
+++ gate.js
@@ -10,5 +10,26 @@ function Gate(x,y,w,h,col){
     fill(this.col)
     rect(this.x, this.y, this.w, this.h)
   }//end display
+  
+  this.collisionWithKey = function() {
+  
+    //check if we hit the left of any wall
+    if (this.y <= g.y + g.h && this.y + this.h >= g.y && this.x <= g.x + g.w && this.x >= g.x) {
+      this.x += 5
+    }
+    //check if we hit the right of any wall
+    if (this.y <= g.y + g.h && this.y + this.h >= g.y && this.x + this.w >= g.x && this.x <= g.x + g.w) {
+      this.x -= 5
+    }
+    //check if we hit the bottom of any wall
+    if (this.x <= g.x + g.w && this.x + this.w >= g.x && this.y <= g.y + g.h && this.y >= g.y) {
+      this.y += 5;
+    }
+    //check if we hit the tothis of any wall
+    if (this.x <= g.x + g.w && this.x + this.w >= g.x && this.y + this.h >= g.y && this.y <= g.y + g.h) {
+      this.y -= 5;
+    }
+
+}//end colision
  
 }//end player
