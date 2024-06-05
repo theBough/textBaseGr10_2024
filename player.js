b33a9fa544e4bfc6d3d90b4f6787a47ff4ac800b
corbin Boughen <50217007+theBough@users.noreply.github.com>
2024-06-05 10:13:38 -0500

    Update player.js
    Switch direction Keys

--- player.js
+++ player.js
@@ -1,23 +1,52 @@
-function Player(x,y,w,h,col,img){
+function Player(x, y, w, h, col, img, room) {
   this.x = x;
   this.y = y;
   this.w = w;
   this.h = h;
   this.col = col;
+  this.room = room;
   //this.img = loadImage(img);
-  
-  this.display = function(){
-    fill(this.col)
-    rect(this.x, this.y, this.w, this.h)
-  }//end display
-  this.update = function(){
-    if(keyIsDown(37)){
-      //the left arrow key is pressed
-      this.x -= 3;
+
+  this.display = function () {
+    fill(this.col);
+    rect(this.x, this.y, this.w, this.h);
+  }; //end display
+  this.update = function () {
+    if (rooms[activeRow][activeColumn] == this.room) {
+      console.log("test")
+      if (keyIsDown(37)) {
+        //the left arrow key is pres
+        this.x += 3;
+      }
+      if (keyIsDown(38)) {
+        //the up arrow key is pressed
+        this.y += 3;
+      }
+      if (keyIsDown(39)) {
+        //the right arrow key is pressed
+        this.x -= 3;
+      }
+      if (keyIsDown(40)) {
+        //the down arrow key is pressed
+        this.y -= 3;
+      }
+    } else {
+      if (keyIsDown(37)) {
+        //the left arrow key is pres
+        this.x -= 3;
+      }
+      if (keyIsDown(38)) {
+        //the up arrow key is pressed
+        this.y -= 3;
+      }
+      if (keyIsDown(39)) {
+        //the right arrow key is pressed
+        this.x += 3;
+      }
+      if (keyIsDown(40)) {
+        //the down arrow key is pressed
+        this.y += 3;
+      }
     }
-    if(keyIsDown(38)){
-      //the up arrow key is pressed
-      this.y -= 3;
-    }
-  }//end update
-}//end player
+  }; //end update
+} //end player
